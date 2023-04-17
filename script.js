8152d76df013b1fdc575fc714b2e81a8461a800f
Kundan Kumar <59140242+kundan-6646@users.noreply.github.com>
2023-04-17 20:17:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 //your code here
 
+function strip (word) {
+    let regex = new RegExp('^(a |the |an )', 'i')
+    return word.replace(regex, '').trim()
+}
+
+const sortedTouristSpots  = touristSpots .sort((a, b) => (strip(a) > strip(b)) ? 1 : -1)
+
+document.querySelector('#bands').innerHTML = 
+    sortedBands.map(item => `<li>${item}</li>`).join('')
